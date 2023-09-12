cd8413b22d6c422ad6542036666195eb076dfada
birendrabohara1230 <birendrabohara2074@gmail.com>
2023-09-12 21:31:45 +0545

    Add images

--- Data/questionswithsolution.js
+++ Data/questionswithsolution.js
@@ -2,6 +2,14 @@ const questionswithsolution = [
     {
         category: "sum",
         imgUrl: "../images/4multiplesum.png"
+    },
+    {
+        category: "evenorodd",
+        imgUrl: "../images/evenorodd.png"
+    },
+    {
+        category: "evenorodd",
+        imgUrl: "../images/evenoroddsub.png"
     }
 ];
 
