6ed20cb4bba096cc06a4d34147d47bb898a54942
birendrabohara1230 <birendrabohara2074@gmail.com>
2023-09-24 08:08:34 +0545

    Add function procedure content

--- Data/modularProgramming.js
+++ Data/modularProgramming.js
@@ -8,6 +8,10 @@ const modularProgramming = [
         qns:"Sub Procedure",
         ans:"This is one of the way of achieving modular programming in Qbasic.<br><b>How to write program using Sub Procedure in Qbasic</b><br>For that we have follow three steps<br><b>a)</b>Declaration of Sub Procedure<br><b>b)</b>Definition of Sub Procedure<br><b>c)</b>Calling Sub Procedure<br><a href='../images/subproceduresyntax.png' target='_blank'><img src='../images/subproceduresyntax.png' width='70%'></a><br><b>Note</b><ol><li>The word which are written in blue color will always remain same, these word are part of syntax.</li><li>Name of sub procedure is your choice, but follow the rule of writting variable name.</li><li>Definition of sub procedure is always remains below end.</li></ol>",
         exp: "<b>Example1 </b><br><a href='../images/subexample.png' target='_blank'><img src='../images/subexample.png' width='70%'></a><br>When we run this program, this will be output look like <br><a href='../images/subexampleoutputscreen.png' target='_blank'><img src='../images/subexampleoutputscreen.png' width='70%'></a><br>As I already told you, modular programming means next way of writing program else otherwise everything(logics or techniques) will remain same as of normal way of doing programs.<br><b>Example 2</b><br><a href='../images/subexample1.png' target='_blank'><img src='../images/subexample1.png' width='70%'></a><br>Just look normal way of writing same program and compare both them, You will find <b>CORE LOGIC</b> in the both program is same.The only difference is we divided normal program into three part using <b>Sub Procedure</b><br><a href='../images/evenorodd.png' target='_blank'><img src='../images/evenorodd.png' width='70%'></a>"
+    },
+    {
+        qns: "Function Procedure",
+        ans: "Function procedure are same as of <b>Sub Procedure </b>, but only difference is when we call function procedure always it returns value and syntax for calling function procedure is also different.<br><a href='../images/functionproceduresyntax.png' target='_blank'><img src='../images/functionproceduresyntax.png' width='70%'></a>"
     }
 ];
 
