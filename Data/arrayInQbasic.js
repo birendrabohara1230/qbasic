33049171258d3cd60103b8ab8ca60c0132606dfe
Birendra Bohara <birendrabohara2074@gmail.com>
2023-10-06 06:29:51 +0545

    Adding Content

---
+++ Data/arrayInQbasic.js
@@ -0,0 +1,6 @@
+const arrayInQbasic = [
+    {
+        qns: "Introduction to Array",
+        ans: "As we know that variable are used to store data, but one piece of data at a time.<br><a href='../images/introarray.png' target='_blank'><img src='../images/introarray.png' width='40%'></a> "
+    }
+]
