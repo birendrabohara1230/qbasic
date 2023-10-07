ab3a198f40038a8aa2cb824e6c214d0a3a135288
Birendra Bohara <birendrabohara2074@gmail.com>
2023-10-08 05:13:21 +0545

    Array Content added

--- Data/arrayInQbasic.js
+++ Data/arrayInQbasic.js
@@ -3,7 +3,10 @@ const arrayInQbasic = [
         qns: "Introduction to Array",
         ans: "As we know that variable are used to store data, but one piece of data at a time.<br><a href='./images/introarray.png' target='_blank'><img src='./images/introarray.png' width='40%'></a>",
         exp: "<h3>Array</h3>Array is variable that has a capacity to hold more than one data at a time under the single variable name.<br>In order to use array we have follow some steps<ol><li>First we have to declare array for that we have use <b>dim</b> keyword</li><li>Second one is we have to mention the size of array i.e. how many data we have to store in that array variable</li></ol>Look at the example<br><a href='./images/arraydeclaration.png' target='_blank'><img src='./images/arraydeclaration.png' width='40%'></a><br><a href='./images/arrayexample1.png' target='_blank'><img src='./images/arrayexample1.png' width='40%'></a><br><a href='./images/arrayexample2.png' target='_blank'><img src='./images/arrayexample2.png' width='40%'></a>"
-
+    },
+    {
+        qns: "Inserting and Displaying data of Array",
+        ans: "<a href='./images/arrayexample3.png' target='_blank'><img src='./images/arrayexample3.png' width='40%'></a>"
     }
 ]
 
